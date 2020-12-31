ded7be6aa298cc5eaf7cc94678fba6a7148a7572
Phil Ross <phil.ross@gmail.com>
2020-12-31 16:29:44 +0000

    Use updated 1.9.3 Windows build to fix issues with rb-readline.

--- dist/index.js
+++ dist/index.js
@@ -28209,7 +28209,7 @@ __webpack_require__.r(__webpack_exports__);
 const versions = {
   "x86": {
     "2.0.0": "https://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p648-i386-mingw32.7z",
-    "1.9.3": "https://github.com/philr/rubyinstaller/releases/download/1.9.3-p551-openssl-tls-1.1-1.2/ruby-1.9.3-p551-i386-mingw32.7z"
+    "1.9.3": "https://github.com/philr/rubyinstaller/releases/download/1.9.3-p551-openssl-tls-1.1-1.2-2/ruby-1.9.3-p551-i386-mingw32.7z"
   },
   "x64": {
     "2.0.0": "https://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p648-x64-mingw32.7z"
