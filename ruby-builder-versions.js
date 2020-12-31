00c8bcf2de87b6f6762cb4e50f66d6558f67eff9
Phil Ross <phil.ross@gmail.com>
2020-12-31 19:13:32 +0000

    Add JRuby 1.7.27 and 9.0.5.0.

--- ruby-builder-versions.js
+++ ruby-builder-versions.js
@@ -4,6 +4,10 @@ export function getVersions(platform) {
       "1.8.7-p375",
       "1.9.3-p551",
       "2.0.0-p648"
+    ],
+    "jruby": [
+      "1.7.27",
+      "9.0.5.0"
     ]
   }
 
--- test_subprocess.rb
+++ test_subprocess.rb
@@ -7,7 +7,17 @@ $stderr = StringIO.new
 begin
   ruby = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'] + RbConfig::CONFIG['EXEEXT'])
   system ruby, "-e", "p :OK"
-  out = $stderr.string
+  if RUBY_ENGINE == 'jruby'
+    out = []
+    $stderr.rewind
+    $stderr.each_line do |s|
+      # Ignore warnings from the JVM.
+      out << s unless s =~ /\AWARNING: .*([Ii]llegal|reporting)/
+    end
+    out = out.join('')
+  else
+    out = $stderr.string
+  end
 ensure
   $stderr = STDERR
 end
