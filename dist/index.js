e1d05b1c57c5495da572efac512b2abc2bb600cf
Phil Ross <phil.ross@gmail.com>
2021-10-02 20:15:45 +0100

    Update to official download sources for Ruby 2.0.0 on Windows.
    2.0.0 builds have now been made available on GitHub:
    https://github.com/oneclick/rubyinstaller/releases/tag/ruby-2.0.0-p648

--- dist/index.js
+++ dist/index.js
@@ -28251,11 +28251,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "versions", function() { return versions; });
 const versions = {
   "x86": {
-    "2.0.0": "https://github.com/philr/rubyinstaller/releases/download/2.0.0-p648/ruby-2.0.0-p648-i386-mingw32.7z",
-    "1.9.3": "https://github.com/philr/rubyinstaller/releases/download/1.9.3-p551-openssl-tls-1.1-1.2-2/ruby-1.9.3-p551-i386-mingw32.7z"
+    "2.0.0": "https://github.com/oneclick/rubyinstaller/releases/download/ruby-2.0.0-p648/ruby-2.0.0-p648-i386-mingw32.7z",
+    "1.9.3": "https://github.com/philr/rubyinstaller/releases/download/1.9.3-p551-openssl-tls-1.1-1.2/ruby-1.9.3-p551-i386-mingw32.7z"
   },
   "x64": {
-    "2.0.0": "https://github.com/philr/rubyinstaller/releases/download/2.0.0-p648/ruby-2.0.0-p648-x64-mingw32.7z"
+    "2.0.0": "https://github.com/oneclick/rubyinstaller/releases/download/ruby-2.0.0-p648/ruby-2.0.0-p648-x64-mingw32.7z"
   }
 }
 
